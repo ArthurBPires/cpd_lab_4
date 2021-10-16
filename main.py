6c95b4343e4fb3b74df3fbb6f8c4710897333103
ArthurBPires <arthurbp2001@outlook.com>
2021-10-16 03:28:08 -0300

    removed main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-#Função de hash tem que mapear de 0 a M-1
-#Resolução de conflitos por endereçamento fechado
-#Implementar função de inserção
-#Implementar função de busca que retorna -1 se não encontrar ou um número positivo indicando o número de busca necessárias para encontra-lo.
-#Fazer cada teste, função de busca do teste deve retornar n+1, sendo n o número de colisões. Caso não encontre, retorna o número máximo de colisões.
-#Escrever output em arquivos...
-
-hashFunction = lambda string,M : string % M
