b00d8fb7e557a6e99959a5c4e9643e1e61062eb0
ArthurBPires <arthurbp2001@outlook.com>
2021-10-17 15:57:34 -0300

    M is now defined as an int variable

--- main.c
+++ main.c
@@ -11,27 +11,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define M 503 //503, 2503, 5003 e 7507
-
 typedef struct {
     char key[71]; //Hash table armazena os nomes de uma lista de contatos.
     struct HT *next;
 } HT;
 
-unsigned long long hash(char* name) {
+unsigned long long hash(char* name, int m) {
     unsigned long long hash = 0;
     int p = 53; //31 se só lower/upper case, 53 se os dois
     
     for (int i = 0; i < strlen(name); i++) {
-        hash = (hash * p + (name[i] - 'A' + 1)) % M;
+        hash = (hash * p + (name[i] - 'A' + 1)) % m;
     }
     return hash;
 }
 
 int main() {
-    HT hashTable[M];
+    int m = 503; //503, 2503, 5003 e 7507
+    HT hashTable[m];
     char name[] = "Arthur Brackmann Pires";
 
-    printf("%llu", hash(name));
+    printf("%llu", hash(name,m));
 }
 
