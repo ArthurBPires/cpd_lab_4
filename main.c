e55cb82398df6b1b7aae8fc8703f3165111b8ba1
ArthurBPires <arthurbp2001@outlook.com>
2021-10-16 23:01:52 -0300

    Added hash function and defined hash table structure

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * - Função de hash tem que mapear de 0 a M-1
  * - Resolução de conflitos por endereçamento fechado
  * - Implementar função de inserção
- * - Implementar função de busca que retorna -1 se não encontrar ou um número positivo indicando o número de busca necessárias para encontra-lo.
+ * - (ignorar este)Implementar função de busca que retorna -1 se não encontrar ou um número positivo indicando o número de busca necessárias para encontra-lo.
  * - Fazer cada teste, função de busca do teste deve retornar n+1, sendo n o número de colisões. Caso não encontre, retorna o número máximo de colisões.
  * - Escrever output em arquivos...
  */
@@ -13,19 +13,25 @@
 
 #define M 503 //503, 2503, 5003 e 7507
 
-int hashFunction(char* name) {
-    //return name % M;
+typedef struct {
+    char key[71]; //Hash table armazena os nomes de uma lista de contatos.
+    struct HT *next;
+} HT;
+
+unsigned long long hash(char* name) {
+    unsigned long long hash = 0;
+    int p = 53; //31 se só lower/upper case, 53 se os dois
+    
+    for (int i = 0; i < strlen(name); i++) {
+        hash = (hash * p + (name[i] - 'A' + 1)) % M;
+    }
+    return hash;
 }
 
-int main() { //Tentativa de função alternativa para função de hash: ficou complicada d+ para ser útil.
-    const char *name = "Arthur Brackmann Pires";
-    static unsigned long long int n[12];
-    int i,k=0;
-    
-    for(i=0; i<strlen(name); i++) {
-        n[k] = ((unsigned long long int)name[i])<<(8*i) | n[k];
-        if((i+1) % 8 == 0) k++;
-    }
-    printf("%s", n);
+int main() {
+    HT hashTable[M];
+    char name[] = "Arthur Brackmann Pires";
+
+    printf("%llu", hash(name));
 }
 
